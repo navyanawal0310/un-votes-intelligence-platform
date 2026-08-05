39fbe0d4a155f465aac75a7470ce988ce42a485a
navyanawal0310 <navyanawal4396@gmail.com>
2026-08-05 09:39:21 +0530

    feat: add dataset profiling tool

---
+++ packages/shared/constants.py
@@ -0,0 +1,17 @@
+DATASET_PATH = "data/raw/sample/UN DATA.csv"
+
+REPORT_DIR = "reports/profiling"
+
+METADATA_COLUMNS = [
+    "Council",
+    "Date",
+    "Title",
+    "Resolution",
+    "TOTAL VOTES",
+    "NO-VOTE COUNT",
+    "ABSENT COUNT",
+    "NO COUNT",
+    "YES COUNT",
+    "Link",
+    "token",
+]
